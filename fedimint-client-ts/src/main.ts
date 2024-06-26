ae68810841f1563723585f8fa5d1eaf19445f860
Alex Lewin <alexlwn123@gmail.com>
2024-06-26 13:32:59 -0400

    cleanup

--- fedimint-client-ts/src/main.ts
+++ fedimint-client-ts/src/main.ts
@@ -5,8 +5,6 @@ console.log("fedimint-client-ts", FedimintWallet);
 const $ = document.querySelector.bind(document);
 
 // Joining a federation
-
-//
 const inviteCode =
   "fed11qgqrgvnhwden5te0v9k8q6rp9ekh2arfdeukuet595cr2ttpd3jhq6rzve6zuer9wchxvetyd938gcewvdhk6tcqqysptkuvknc7erjgf4em3zfh90kffqf9srujn6q53d6r056e4apze5cw27h75";
 
@@ -26,7 +24,7 @@ FedimintWallet.initFedimint()
 /** refetch balance */
 
 async function refreshBalance() {
-  const balance = $(".balance");
+  const balance: HTMLParagraphElement | null = $(".balance");
   if (!balance) return;
   const bal: number = await fed.getBalance();
   console.log("balance", bal);
@@ -38,8 +36,11 @@ window.refetch = refreshBalance;
 
 $(".ecash-submit")?.addEventListener("click", async (event) => {
   event.preventDefault();
-  const ecash: string = $(".ecash-input")?.value;
+  const ecashInput: HTMLInputElement | null = $(".ecash-input");
+  if (!ecashInput) return;
+  const ecash: string = ecashInput.value;
   if (!ecash) return;
+
   console.log("ecash note", ecash);
   const res = await fed.reissueNotes(ecash);
   console.log("redeemed ecash", res);
@@ -48,8 +49,11 @@ $(".ecash-submit")?.addEventListener("click", async (event) => {
 
 $(".lightning-submit")?.addEventListener("click", async (event) => {
   event.preventDefault();
-  const invoice: string = $(".lightning-input")?.value;
+  const invoiceInput: HTMLInputElement | null = $(".lightning-input");
+  if (!invoiceInput) return;
+  const invoice: string = invoiceInput.value;
   if (!invoice) return;
+
   console.log("lightning invoice", invoice);
   const res = await fed.payInvoice(invoice);
   console.log("paid lightning invoice", res);
