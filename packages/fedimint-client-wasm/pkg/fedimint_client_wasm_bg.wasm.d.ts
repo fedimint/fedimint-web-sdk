111b97e71829d4ebbe6361f199d211a56e3d4652
Alex Lewin <alexlwn123@gmail.com>
2024-06-27 17:31:32 -0400

    readded wasm files

---
+++ packages/fedimint-client-wasm/pkg/fedimint_client_wasm_bg.wasm.d.ts
@@ -0,0 +1,30 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_wasmclient_free(a: number): void;
+export function wasmclient_open(): number;
+export function wasmclient_join_federation(a: number, b: number): number;
+export function wasmclient_rpc(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number): number;
+export function ring_core_0_17_8_bn_mul_mont(a: number, b: number, c: number, d: number, e: number, f: number): void;
+export function rustsecp256k1zkp_v0_8_0_default_illegal_callback_fn(a: number, b: number): void;
+export function rustsecp256k1zkp_v0_8_0_default_error_callback_fn(a: number, b: number): void;
+export function rustsecp256k1_v0_6_1_context_create(a: number): number;
+export function rustsecp256k1_v0_6_1_context_destroy(a: number): void;
+export function rustsecp256k1_v0_6_1_default_illegal_callback_fn(a: number, b: number): void;
+export function rustsecp256k1_v0_6_1_default_error_callback_fn(a: number, b: number): void;
+export function rustsecp256k1_v0_8_1_context_create(a: number): number;
+export function rustsecp256k1_v0_8_1_context_destroy(a: number): void;
+export function rustsecp256k1_v0_8_1_default_illegal_callback_fn(a: number, b: number): void;
+export function rustsecp256k1_v0_8_1_default_error_callback_fn(a: number, b: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h097bd3e9f2099494(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hddb6488072da84a0(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hf0ebb8a89d864501(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h97cb9335b90d3a95(a: number, b: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h03112f63462c5f4e(a: number, b: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hd94b7bbe8f82255c(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h701c21cc59581b3c(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h7293b85f43358e2f(a: number, b: number, c: number, d: number): void;
