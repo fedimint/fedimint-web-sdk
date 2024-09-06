f4e8dc29d1ee5e7281da8d4e452a8a131a2b9b0e
kody.low <kodylow7@gmail.com>
2024-09-06 16:19:18 -0700

    chore: nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Fedimint Web SDK Typescript Monorepo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            nativeBuildInputs = [ pkgs.nodePackages.pnpm ];
+            shellHook = ''
+              pnpm install
+            '';
+          };
+        };
+      });
+}
