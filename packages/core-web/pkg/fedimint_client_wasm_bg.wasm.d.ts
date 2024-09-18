cf03a9a84c26e3d1b8b79eb11ca21105e712c463
Alex Lewin <alexlwn123@gmail.com>
2024-09-18 12:48:35 -0400

    feat: WASM INSTANTIATION HOLY HELL FINALLY WORKS

---
+++ packages/core-web/pkg/fedimint_client_wasm_bg.wasm.d.ts
@@ -0,0 +1,110 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory
+export function __wbg_wasmclient_free(a: number): void
+export function __wbg_rpchandle_free(a: number): void
+export function rpchandle_cancel(a: number): void
+export function wasmclient_open(a: number, b: number): number
+export function wasmclient_join_federation(
+  a: number,
+  b: number,
+  c: number,
+  d: number,
+): number
+export function wasmclient_rpc(
+  a: number,
+  b: number,
+  c: number,
+  d: number,
+  e: number,
+  f: number,
+  g: number,
+  h: number,
+): number
+export function ring_core_0_17_8_bn_mul_mont(
+  a: number,
+  b: number,
+  c: number,
+  d: number,
+  e: number,
+  f: number,
+): void
+export function rustsecp256k1zkp_v0_8_0_default_illegal_callback_fn(
+  a: number,
+  b: number,
+): void
+export function rustsecp256k1zkp_v0_8_0_default_error_callback_fn(
+  a: number,
+  b: number,
+): void
+export function rustsecp256k1_v0_6_1_context_create(a: number): number
+export function rustsecp256k1_v0_6_1_context_destroy(a: number): void
+export function rustsecp256k1_v0_6_1_default_illegal_callback_fn(
+  a: number,
+  b: number,
+): void
+export function rustsecp256k1_v0_6_1_default_error_callback_fn(
+  a: number,
+  b: number,
+): void
+export function rustsecp256k1_v0_8_1_context_create(a: number): number
+export function rustsecp256k1_v0_8_1_context_destroy(a: number): void
+export function rustsecp256k1_v0_8_1_default_illegal_callback_fn(
+  a: number,
+  b: number,
+): void
+export function rustsecp256k1_v0_8_1_default_error_callback_fn(
+  a: number,
+  b: number,
+): void
+export function __wbindgen_malloc(a: number, b: number): number
+export function __wbindgen_realloc(
+  a: number,
+  b: number,
+  c: number,
+  d: number,
+): number
+export const __wbindgen_export_2: WebAssembly.Table
+export function __wbindgen_add_to_stack_pointer(a: number): number
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h8697e16cd1c64495(
+  a: number,
+  b: number,
+  c: number,
+  d: number,
+): void
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h31a94cc9a05d5ca0(
+  a: number,
+  b: number,
+  c: number,
+): void
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h4d11ec113460b95d(
+  a: number,
+  b: number,
+): void
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__he7056307c6986185(
+  a: number,
+  b: number,
+  c: number,
+  d: number,
+): void
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h5b16159cdfa166b0(
+  a: number,
+  b: number,
+): void
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hebf1bd391d12b3cb(
+  a: number,
+  b: number,
+  c: number,
+): void
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h09ee3e3abd173580(
+  a: number,
+  b: number,
+): void
+export function __wbindgen_free(a: number, b: number, c: number): void
+export function __wbindgen_exn_store(a: number): void
+export function wasm_bindgen__convert__closures__invoke2_mut__h8bdaa9faeb7d5075(
+  a: number,
+  b: number,
+  c: number,
+  d: number,
+): void
--- packages/core-web/src/FedimintWallet.ts
+++ packages/core-web/src/FedimintWallet.ts
@@ -11,7 +11,7 @@ import {
   ModuleKind,
   GatewayInfo,
   CancelFunction,
-} from './types/wallet.js'
+} from './types/wallet'
 
 const DEFAULT_CLIENT_NAME = 'fm-default' as const
 
@@ -48,7 +48,8 @@ export class FedimintWallet {
       const requestId = this.getNextRequestId()
       this.requestCallbacks.set(requestId, (data) => {
         this.requestCallbacks.delete(requestId)
-        resolve(data)
+        if (data.data) resolve(data.data)
+        else if (data.error) reject(data.error)
       })
       try {
         this.worker!.postMessage({ type, payload, requestId })
@@ -59,7 +60,7 @@ export class FedimintWallet {
   }
 
   // Setup
-  async initialize() {
+  initialize() {
     if (this.initPromise) return this.initPromise
     this.worker = new Worker(new URL('./worker.js', import.meta.url))
     this.worker.onmessage = this.handleWorkerMessage.bind(this)
@@ -72,7 +73,7 @@ export class FedimintWallet {
     const streamCallback = this.requestCallbacks.get(requestId)
     // TODO: Handle errors... maybe have another callbacks list for errors?
     if (streamCallback) {
-      streamCallback(data.data)
+      streamCallback(data) // {data: something} OR {error: something}
     }
   }
 
