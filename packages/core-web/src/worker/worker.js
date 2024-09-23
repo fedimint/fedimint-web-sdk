2f722a417a67654ea6ae7ceef30f78091328cd7c
Alex Lewin <alexlwn123@gmail.com>
2024-09-23 13:41:38 -0400

    feat: add debug logging

--- packages/core-web/src/worker/worker.js
+++ packages/core-web/src/worker/worker.js
@@ -12,74 +12,81 @@ const handleFree = (requestId) => {
   streamCancelMap.delete(requestId)
 }
 
+console.log('Worker - init')
+
 self.onmessage = async (event) => {
   const { type, payload, requestId } = event.data
 
-  if (type === 'init') {
-    WasmClient = (await import('@fedimint/fedimint-client-wasm')).WasmClient
-    self.postMessage({ type: 'initialized', data: {}, requestId })
-  } else if (type === 'open') {
-    const { clientName } = payload
-    client = (await WasmClient.open(clientName)) || null
-    self.postMessage({
-      type: 'open',
-      data: { success: !!client },
-      requestId,
-    })
-  } else if (type === 'join') {
-    const { inviteCode, clientName: joinClientName } = payload
-    try {
-      client = await WasmClient.join_federation(joinClientName, inviteCode)
+  try {
+    if (type === 'init') {
+      WasmClient = (await import('@fedimint/fedimint-client-wasm')).WasmClient
+      self.postMessage({ type: 'initialized', data: {}, requestId })
+    } else if (type === 'open') {
+      const { clientName } = payload
+      client = (await WasmClient.open(clientName)) || null
       self.postMessage({
-        type: 'join',
+        type: 'open',
         data: { success: !!client },
         requestId,
       })
-    } catch (e) {
-      self.postMessage({ type: 'error', error: e.message, requestId })
-    }
-  } else if (type === 'rpc') {
-    const { module, method, body } = payload
-    console.log('RPC received', module, method, body)
-    if (!client) {
+    } else if (type === 'join') {
+      const { inviteCode, clientName: joinClientName } = payload
+      try {
+        client = await WasmClient.join_federation(joinClientName, inviteCode)
+        self.postMessage({
+          type: 'join',
+          data: { success: !!client },
+          requestId,
+        })
+      } catch (e) {
+        self.postMessage({ type: 'error', error: e.message, requestId })
+      }
+    } else if (type === 'rpc') {
+      const { module, method, body } = payload
+      console.log('RPC received', module, method, body)
+      if (!client) {
+        self.postMessage({
+          type: 'error',
+          error: 'WasmClient not initialized',
+          requestId,
+        })
+        return
+      }
+      const rpcHandle = await client.rpc(
+        module,
+        method,
+        JSON.stringify(body),
+        (res) => {
+          console.log('RPC response', requestId, res)
+          const data = JSON.parse(res)
+          self.postMessage({ type: 'rpcResponse', requestId, ...data })
+
+          if (data.end !== undefined) {
+            // Handle stream ending
+            const handle = streamCancelMap.get(requestId)
+            handle?.free()
+          }
+        },
+      )
+      streamCancelMap.set(requestId, rpcHandle)
+    } else if (type === 'unsubscribe') {
+      const rpcHandle = streamCancelMap.get(requestId)
+      if (rpcHandle) {
+        rpcHandle.cancel()
+        rpcHandle.free()
+        streamCancelMap.delete(requestId)
+      }
+    } else {
       self.postMessage({
         type: 'error',
-        error: 'WasmClient not initialized',
+        error: 'Unknown message type',
         requestId,
       })
-      return
     }
-    const rpcHandle = await client.rpc(
-      module,
-      method,
-      JSON.stringify(body),
-      (res) => {
-        console.log('RPC response', requestId, res)
-        const data = JSON.parse(res)
-        self.postMessage({ type: 'rpcResponse', requestId, ...data })
-
-        if (data.end !== undefined) {
-          // Handle stream ending
-          const handle = streamCancelMap.get(requestId)
-          handle?.free()
-        }
-      },
-    )
-    streamCancelMap.set(requestId, rpcHandle)
-  } else if (type === 'unsubscribe') {
-    const rpcHandle = streamCancelMap.get(requestId)
-    if (rpcHandle) {
-      rpcHandle.cancel()
-      rpcHandle.free()
-      streamCancelMap.delete(requestId)
-    }
-  } else {
-    self.postMessage({
-      type: 'error',
-      error: 'Unknown message type',
-      requestId,
-    })
+  } catch (e) {
+    console.error('ERROR', e)
+    self.postMessage({ type: 'error', error: e, requestId })
   }
 }
 
-self.postMessage({ type: 'init', data: {} })
+// self.postMessage({ type: 'init', data: {} })
