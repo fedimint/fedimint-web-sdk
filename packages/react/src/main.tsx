de756e5b0c14865e8b6b855dd374872525c84728
Alex Lewin <alexlwn123@gmail.com>
2024-10-25 15:41:15 +0100

    chore: clean up hooks demo code

--- packages/react/src/main.tsx
+++ packages/react/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import { FedimintWalletProvider, setupFedimintWallet } from '../lib/contexts'
 
+// Setup singleton client
 setupFedimintWallet({
   lazy: false,
   debug: true,
@@ -11,6 +12,8 @@ setupFedimintWallet({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    {/* Wrap the app in the FedimintWalletProvider */}
+
     <FedimintWalletProvider>
       <App />
     </FedimintWalletProvider>
