624366ff1d7fc9ee117d19fc81867b483f323d8b
Alex Lewin <alexlwn123@gmail.com>
2025-05-23 14:30:17 -0400

    fix: types on walletSummary to match types in rust

--- packages/core-web/src/types/wallet.ts
+++ packages/core-web/src/types/wallet.ts
@@ -103,9 +103,9 @@ type TxOutputSummary = {
 
 type WalletSummary = {
   spendable_utxos: TxOutputSummary[]
-  unsigned_peg_out_utxos: TxOutputSummary[]
+  unsigned_peg_out_txos: TxOutputSummary[]
   unsigned_change_utxos: TxOutputSummary[]
-  unconfirmed_peg_out_utxos: TxOutputSummary[]
+  unconfirmed_peg_out_txos: TxOutputSummary[]
   unconfirmed_change_utxos: TxOutputSummary[]
 }
 
