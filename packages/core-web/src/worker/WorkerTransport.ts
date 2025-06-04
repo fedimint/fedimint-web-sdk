b62bc1eeb1e0b3c507eaca44eadc1b6a68ec6185
Alex Lewin <alexlwn123@gmail.com>
2025-06-03 21:48:45 -0400

    chore: fixup worker code for ng

--- packages/core-web/src/worker/WorkerTransport.ts
+++ packages/core-web/src/worker/WorkerTransport.ts
@@ -1,6 +1,6 @@
 import type { RpcResponseFull, RpcRequestFull } from '../types'
 import { logger } from '../utils/logger'
-import type { RpcTransport, RpcTransportInit } from '../rpc'
+import type { RpcTransport } from '../rpc'
 
 class WebWorkerTransport implements RpcTransport {
   constructor(private worker: Worker) {}
@@ -14,34 +14,33 @@ class WebWorkerTransport implements RpcTransport {
   }
 }
 
-export class WebWorkerTransportInit implements RpcTransportInit {
-  async init(
-    onRpcResponse: (response: RpcResponseFull) => void,
-  ): Promise<RpcTransport> {
-    const worker = new Worker(new URL('./worker.js', import.meta.url), {
-      type: 'module',
-    })
+export const createWebWorkerTransport = async (
+  onRpcResponse: (response: RpcResponseFull) => void,
+): Promise<WebWorkerTransport> => {
+  const worker = new Worker(new URL('./worker.js', import.meta.url), {
+    type: 'module',
+  })
 
-    await new Promise<void>((resolve, reject) => {
-      const handleInit = (event: MessageEvent) => {
-        const response = event.data
-        if (response.type === 'init_error') {
-          reject(new Error(response.error))
-        } else if (response.type === 'init_success') {
-          logger.info('WebWorker instantiated')
-          resolve()
-        }
+  await new Promise<void>((resolve, reject) => {
+    const handleInit = (event: MessageEvent) => {
+      const response = event.data
+      if (response.type === 'init_error') {
+        reject(new Error(response.error))
+      } else if (response.type === 'init_success') {
+        logger.info('WebWorker instantiated')
+        resolve()
       }
-
-      worker.onmessage = handleInit
-      worker.postMessage({
-        type: 'init',
-      })
-    })
-
-    worker.onmessage = (event: MessageEvent) => {
-      onRpcResponse(event.data as RpcResponseFull)
     }
-    return new WebWorkerTransport(worker)
+
+    worker.onmessage = handleInit
+    worker.postMessage({
+      type: 'init',
+      request_id: 0,
+    })
+  })
+
+  worker.onmessage = (event: MessageEvent) => {
+    onRpcResponse(event.data as RpcResponseFull)
   }
+  return new WebWorkerTransport(worker)
 }
