2efe2465c31e0269d4810f6a5549951496454b90
Alex Lewin <alexlwn123@gmail.com>
2025-06-10 10:49:17 -0400

    feat: build wasm web package too

--- scripts/build_wasm.sh
+++ scripts/build_wasm.sh
@@ -7,3 +7,4 @@ nix build -L .#wasmBundle
 
 echo "Copying WASM files..."
 cp result/share/fedimint-client-wasm/fedimint_* packages/wasm-bundler/
+cp result/share/fedimint-client-wasm-web/fedimint_* packages/wasm-web/
