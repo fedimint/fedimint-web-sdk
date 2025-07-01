5a223bde4b812e49ce000046b6c5411db8fc3e3e
Alex Lewin <43247027+alexlwn123@users.noreply.github.com>
2025-07-01 16:36:28 -0400

    chore: add separate nix input for wasm vs devimint (#169)
    * chore: add separate nix input for wasm vs devimint
    
    * chore: fix size job

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     fedimint = {
+      # Devimint input - Should point to a release tag, as it doesn't need to be updated often.
+      url = "github:fedimint/fedimint/v0.7.2";
+    };
+    fedimint-wasm = {
       url = "github:fedimint/fedimint?rev=ba238118bf5b204bc73c1113b6cadd62bca4e66c";
     };
   };
@@ -10,6 +14,7 @@
       self,
       flake-utils,
       fedimint,
+      fedimint-wasm,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -52,7 +57,7 @@
           };
         };
         packages = {
-          wasmBundle = fedimint.packages.${system}.wasmBundle;
+          wasmBundle = fedimint-wasm.packages.${system}.wasmBundle;
         };
       }
     );
