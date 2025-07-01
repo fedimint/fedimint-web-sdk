59a73bf5704e7e1d29538cde12208cb7c2e3274a
Alex Lewin <alexlwn123@gmail.com>
2025-07-01 13:54:57 -0400

    chore: add separate nix input for wasm vs devimint

--- scripts/build_wasm.sh
+++ scripts/build_wasm.sh
@@ -8,3 +8,7 @@ nix build -L .#wasmBundle
 echo "Copying WASM files..."
 cp result/share/fedimint-client-wasm/fedimint_* packages/wasm-bundler/
 cp result/share/fedimint-client-wasm-web/fedimint_* packages/wasm-web/
+
+# Let's future builds replace the existing files
+chmod u+w packages/wasm-bundler/fedimint_*
+chmod u+w packages/wasm-web/fedimint_*
