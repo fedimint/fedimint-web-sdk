e8448d4cd9f82d21c860eab0576df0a1abf389d1
Alex Lewin <alexlwn123@gmail.com>
2025-07-22 13:44:58 -0400

    chore: bump vitest to v3.2.4

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,4 @@
+import wasm from 'vite-plugin-wasm'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
@@ -7,6 +8,45 @@ export default defineConfig({
       provider: 'v8',
       include: ['packages/**/*.ts'],
     },
+    projects: [
+      {
+        plugins: [wasm()],
+        test: {
+          environment: 'happy-dom',
+          name: 'core',
+          include: ['packages/core-web/**/*.test.ts'],
+          exclude: ['packages/create-fedimint-app/**/*.test.ts'],
+          browser: {
+            enabled: true,
+            provider: 'playwright',
+            ui: false, // no ui for the core library
+            api: {
+              port: 63315,
+            },
+            screenshotFailures: false,
+            instances: [
+              {
+                browser: 'chromium',
+                headless: true,
+              },
+            ],
+          },
+          env: {
+            FAUCET: `http://localhost:15243`,
+          },
+        },
+      },
+      {
+        test: {
+          name: 'cli',
+          environment: 'happy-dom',
+          include: ['packages/create-fedimint-app/__tests__/*.test.ts'],
+          exclude: ['packages/create-fedimint-app/__tests__/subfolder'],
+          isolate: true,
+          testTimeout: 20000,
+        },
+      },
+    ],
   },
   optimizeDeps: {
     exclude: ['@fedimint/core-web'],
