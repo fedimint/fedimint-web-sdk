fb4e09c302c369d130fb6c98d86e3c82872c326f
Alex Lewin <43247027+alexlwn123@users.noreply.github.com>
2025-07-22 13:55:23 -0400

    chore: bump vitest to v3.2.4 (#170)

---
+++ .changeset/honest-drinks-fry.md
@@ -0,0 +1,5 @@
+---
+'@fedimint/core-web': patch
+---
+
+Bump vitest framework to v3.2.4
--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,39 +1,42 @@
-import { defineWorkspace } from 'vitest/config'
-import wasm from 'vite-plugin-wasm'
+// import { defineWorkspace } from 'vitest/config'
+// import wasm from 'vite-plugin-wasm'
 
-export default defineWorkspace([
-  {
-    plugins: [wasm()],
-    test: {
-      environment: 'happy-dom',
-      name: 'core',
-      include: ['packages/core-web/**/*.test.ts'],
-      exclude: ['packages/create-fedimint-app/**/*.test.ts'],
-      browser: {
-        enabled: true,
-        headless: true,
-        // name: 'chrome',
-        name: 'chromium',
-        provider: 'playwright',
-        ui: false, // no ui for the core library
-        api: {
-          port: 63315,
-        },
-        screenshotFailures: false,
-      },
-      env: {
-        FAUCET: `http://localhost:15243`,
-      },
-    },
-  },
-  {
-    test: {
-      name: 'cli',
-      environment: 'happy-dom',
-      include: ['packages/create-fedimint-app/__tests__/*.test.ts'],
-      exclude: ['packages/create-fedimint-app/__tests__/subfolder'],
-      isolate: true,
-      testTimeout: 20000,
-    },
-  },
-])
+// export default defineWorkspace([
+//   {
+//     plugins: [wasm()],
+//     test: {
+//       environment: 'happy-dom',
+//       name: 'core',
+//       include: ['packages/core-web/**/*.test.ts'],
+//       exclude: ['packages/create-fedimint-app/**/*.test.ts'],
+//       browser: {
+//         enabled: true,
+//         provider: 'playwright',
+//         ui: false, // no ui for the core library
+//         api: {
+//           port: 63315,
+//         },
+//         screenshotFailures: false,
+//         instances: [
+//           {
+//             browser: 'chromium',
+//             headless: true,
+//           },
+//         ],
+//       },
+//       env: {
+//         FAUCET: `http://localhost:15243`,
+//       },
+//     },
+//   },
+//   {
+//     test: {
+//       name: 'cli',
+//       environment: 'happy-dom',
+//       include: ['packages/create-fedimint-app/__tests__/*.test.ts'],
+//       exclude: ['packages/create-fedimint-app/__tests__/subfolder'],
+//       isolate: true,
+//       testTimeout: 20000,
+//     },
+//   },
+// ])
